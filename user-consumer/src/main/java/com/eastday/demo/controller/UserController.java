59b0c7593017c95dea2e3310347888d86c8c896f
root <827893449@qq.com>
2019-10-14 15:57:14 +0800

    修改

--- user-consumer/src/main/java/com/eastday/demo/controller/UserController.java
+++ user-consumer/src/main/java/com/eastday/demo/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.eastday.demo.controller;
 
 import com.eastday.demo.entity.RetDto;
-import com.eastday.demo.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
